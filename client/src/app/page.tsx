e2b42d5375aa66381c7e5e89a4082944885bd0c5
Adarsh Dubey <dubeyadarshmain@gmail.com>
2025-04-14 16:22:03 +0530

    feat: basic layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,23 @@
+import { ViewContainer } from "@/components/ui/view-container";
+
 export default function Home() {
-	return <h1>Hello world</h1>;
+	return (
+		<main className="">
+			<ViewContainer>
+				{/* biome-ignore lint/suspicious/noCommentText: <explanation> */}
+				<h1 className="">Waitlist // Anon</h1>
+				<p className="">
+					Ever wanted a place to just say things without overthinking?
+					We’re building an anonymous space just for K. K. Wagh
+					students to talk about college, life, or just vibe with
+					random folks from your campus. It’s like a chill gated
+					community, but totally anon.
+				</p>
+				<p className="">
+					We’re launching soon. Hop on the waitlist and be part of it
+					from day one.
+				</p>
+			</ViewContainer>
+		</main>
+	);
 }
